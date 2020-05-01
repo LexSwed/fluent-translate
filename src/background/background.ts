4d4900c7b1efa96201bad6d6dda85c5a23ef0365
Lex Swed <lexswed@gmail.com>
2020-05-01 22:44:23 +0300

    feat: popup

--- src/background/background.ts
+++ src/background/background.ts
@@ -39,15 +39,3 @@ chrome.runtime.onMessage.addListener(
     return true;
   }
 );
-// function(request, sender, sendResponse) {
-//   if (request.contentScriptQuery == "queryPrice") {
-//     var url = "https://another-site.com/price-query?itemId=" +
-//             encodeURIComponent(request.itemId);
-//     fetch(url)
-//         .then(response => response.text())
-//         .then(text => parsePrice(text))
-//         .then(price => sendResponse(price))
-//         .catch(error => ...)
-//     return true;  // Will respond asynchronously.
-//   }
-// });
