877ccefb78e3dde5c84399be264770a5d06f2afb
Lex Swed <lexswed@gmail.com>
2020-05-02 17:28:08 +0300

    delete logging part of privacy policy as no user data is collected in logs

--- packages/www/src/pages/privacy/Privacy.tsx
+++ packages/www/src/pages/privacy/Privacy.tsx
@@ -58,26 +58,6 @@ const Privacy = () => {
         <li>Send entered text to the Translator service</li>
       </ul>
 
-      <h2>Log Files</h2>
-
-      <p>
-        Edge Translate follows a standard procedure of using log files. These
-        files log visitors when they visit websites. All hosting companies do
-        this and a part of hosting services' analytics. The information
-        collected by log files include internet protocol (IP) addresses, browser
-        type, Internet Service Provider (ISP), date and time stamp,
-        referring/exit pages, and possibly the number of clicks. These are not
-        linked to any information that is personally identifiable. The purpose
-        of the information is for analyzing trends, administering the site,
-        tracking users' movement on the website, and gathering demographic
-        information. Our Privacy Policy was created with the help of the{' '}
-        <a href="https://www.privacypolicygenerator.info">
-          Privacy Policy Generator
-        </a>{' '}
-        and the{' '}
-        <a href="https://www.disclaimergenerator.org/">Disclaimer Generator</a>.
-      </p>
-
       <h2>Advertising Partners Privacy Policies</h2>
 
       <p>
