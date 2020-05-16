a9374930b81a32532d59d50ff12c2166059b10c4
Lex Swed <lexswed@gmail.com>
2020-05-16 17:34:05 +0300

    style fixes

--- packages/extension/src/popup/Footer/More.tsx
+++ packages/extension/src/popup/Footer/More.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { MenuButton, Button, Icon, ListBox, Option, Text } from '@fxtrot/edge';
 
+import styles from './styles.css';
+
 const More: React.FC = () => {
   return (
     <MenuButton placement="top-start">
-      <Button tone="transparent" size="xs" aria-label="More">
+      <Button
+        tone="transparent"
+        size="xs"
+        aria-label="More"
+        className={styles.menuButton}
+      >
         <Icon icon="more_vert" />
       </Button>
-      <ListBox>
+      <ListBox className={styles.listbox}>
         <Option
+          className={styles.option}
           onClick={() =>
             chrome.tabs.create({
               url: 'https://alexanderswed.typeform.com/to/sjSxc6',
