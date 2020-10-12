866c8b57c98c0095a125567602209851097adbdd
Lex Swed <lexswed@gmail.com>
2020-10-12 20:54:15 +0200

    chore: update commands

--- packages/extension/src/popup/AppContext.tsx
+++ packages/extension/src/popup/AppContext.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import { useStore } from './store';
 import { StoreContextProvider } from './store/utils';
