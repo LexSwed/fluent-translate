deda845623fec34f7c9b196b625d011735645e3d
Lex Swed <lexswed@gmail.com>
2022-01-06 21:09:17 +0100

    fix: another style update

--- packages/extension/src/popup/Results/Translated.tsx
+++ packages/extension/src/popup/Results/Translated.tsx
@@ -1,5 +1,5 @@
 import { memo } from 'react';
-import { Text, Flex } from '@fxtrot/ui';
+import { Text, Flex, Box } from '@fxtrot/ui';
 
 import { ToLanguageSelect } from '../LanguageSelect';
 import { useTranslation } from '../atoms';
@@ -13,16 +13,18 @@ export const Translated = memo(() => {
   console.log(translation);
   const { text, pronunciation } = translation;
   return (
-    <Flex flow="column" gap="2" cross="stretch">
-      <ToLanguageSelect />
-      <Flex flow="column" gap="1" css={{ pl: '$1' }}>
-        <Text size="md">{text}</Text>
-        {pronunciation && (
-          <Text size="sm" css={{ color: '$textSubtle' }}>
-            {pronunciation}
-          </Text>
-        )}
+    <Box p="$3">
+      <Flex flow="column" gap="2" cross="stretch">
+        <ToLanguageSelect />
+        <Flex flow="column" gap="1" css={{ pl: '$1' }}>
+          {text && <Text size="md">{text}</Text>}
+          {pronunciation && (
+            <Text size="sm" css={{ color: '$textSubtle' }}>
+              {pronunciation}
+            </Text>
+          )}
+        </Flex>
       </Flex>
-    </Flex>
+    </Box>
   );
 });
